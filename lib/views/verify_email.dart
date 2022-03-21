e6f27da9c54f98c436d1dc1959c9d649a2f252c9
malikrasaq <malikrasaq@gmail.com>
2022-03-21 03:34:58 +0100

    Register view Error Handling

--- lib/views/verify_email.dart
+++ lib/views/verify_email.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:notesapp/constants/routes.dart';
 
 class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({Key? key}) : super(key: key);
@@ -13,7 +14,8 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
     return Scaffold(
       appBar: AppBar(title: const Text('Verify Email')),
       body: Column(children: [
-        const Text('Please very email'),
+        const Text('We\'ve sent you an email verification, Please open it to verify your account'),
+        const Text('If you haven\'t received a verification email yet, Press the button below'),
         TextButton(
           onPressed: () async {
             final user = FirebaseAuth.instance.currentUser;
@@ -21,6 +23,11 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
           },
           child: const Text('Send email verification'),
         ),
+        TextButton(onPressed: ()async  {
+          await FirebaseAuth.instance.signOut();
+          Navigator.of(context).pushNamedAndRemoveUntil(registerRoute, (route) => false);
+        }, child: const Text('Restart'),
+        )
       ]),
     );
   }
